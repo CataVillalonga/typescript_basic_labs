8faea146db6d7c3212a38d8b2a3ccb4707b83c30
Cata Villalonga <cata.villalonga21@gmail.com>
2023-01-12 15:27:55 +0100

    CRUD implemented with error basic  handling

--- addons-ts-lab-puppiesApi-main/app.ts
+++ addons-ts-lab-puppiesApi-main/app.ts
@@ -13,7 +13,7 @@ const data = [{
   name:'Bobby', 
   img:'https://images.pexels.com/photos/235805/pexels-photo-235805.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
   birth_date : '01/01/2023',
-  breed:'banana',
+  breed:'Chihuahua',
   fav_activities:['Sleep','Play with the ball']
   },
   {
@@ -21,7 +21,7 @@ const data = [{
   name:'Uni', 
   img:'https://images.pexels.com/photos/1564506/pexels-photo-1564506.jpeg?auto=compress&cs=tinysrgb&w=800',
   birth_date : '01/01/2023',
-  breed:'banana',
+  breed:'Siberian Husky',
   fav_activities:['Swim','Run after cats']
   },
   {
@@ -29,7 +29,7 @@ const data = [{
   name:'Woolfie', 
   img:'https://images.pexels.com/photos/3478875/pexels-photo-3478875.jpeg?auto=compress&cs=tinysrgb&w=800',
   birth_date : '01/01/2023',
-  breed:'banana',
+  breed:'Welsh Corgi',
   fav_activities:['Eat','Look at people walk by']
   },
   {
@@ -45,7 +45,7 @@ const data = [{
   name:'Snow', 
   img:'https://images.pexels.com/photos/3860306/pexels-photo-3860306.jpeg?auto=compress&cs=tinysrgb&w=800',
   birth_date : '01/01/2023',
-  breed:'banana',
+  breed:'Welsh Springer',
   fav_activities:['Chill by the carpet','Watch tv']
   },
   {
@@ -53,7 +53,7 @@ const data = [{
   name:'cafesito', 
   img:'https://images.pexels.com/photos/97863/pexels-photo-97863.jpeg?auto=compress&cs=tinysrgb&w=800',
   birth_date : '01/01/2023',
-  breed:'banana',
+  breed:'Yorkshire Terrier',
   fav_activities:['Fetch the ball','Barck at everything that moves']
   },
 ]
@@ -68,13 +68,26 @@ interface PuppygDetails {
 }
 
 app.get('/api/puppies', (_req: Request, res: Response) => {
-  return res.status(200).json(data);
+
+  return res.location('/api/puppies')
+  .status(200)
+  .json(data);
 });
 
 app.get('/api/puppies/:id', (req: Request, res: Response) => {
-  const id :number = Number(req.params.id)
+  const id = Number(req.params.id)
   const puppy = data.filter(dog => dog.id === id)
-  return res.status(200).json(puppy);
+
+  if (puppy.length !== 0) {
+    return res.location('/api/puppies')
+    .status(200)
+    .json(puppy);
+  }
+  return res.status(404)
+  .json({
+    error: 404,
+    msg: 'Puppy not found'
+  });
 });
 
 app.post('/api/puppies', (req: Request, res: Response) => {
@@ -87,18 +100,61 @@ app.post('/api/puppies', (req: Request, res: Response) => {
     breed,
     fav_activities
   }
-  data.push(newPuppy)
-    if (name && img && birth_date && breed && fav_activities) {
-      return res.status(202).json(data);
+
+    if (name && img && birth_date && breed && fav_activities && typeof name === 'string' &&  
+      typeof img === 'string' &&  typeof birth_date === 'string' &&  typeof breed === 'string') {
+      data.push(newPuppy)
+      return res.location('/api/puppies')
+      .status(202)
+      .json(data);
     }
-    return res.status(400).json('Please provide the right data');
+    return res.status(400)
+    .json({
+      error: 400,
+      msg: 'Bad request, Please provide the right data'
+    });
+});
+app.put('/api/puppies/:id', (req: Request, res: Response) => {
+  const id = Number(req.params.id)
+  const { name, img, birth_date, breed, fav_activities} = req.body
+  const puppy:PuppygDetails | any = data.find(puppy => puppy.id === id);
+
+  if (name && img && birth_date && breed && fav_activities && typeof name === 'string' &&  
+  typeof img === 'string' &&  typeof birth_date === 'string' &&  typeof breed === 'string') {
+    const indexOfPuppy = data.indexOf(puppy);
+    puppy[indexOfPuppy] = {
+      id,
+      name,
+      img,
+      birth_date,
+      breed,
+      fav_activities
+    };
+    return res.location(`/api/presidents/${puppy[indexOfPuppy].id}`)
+    .status(200)
+    .json(puppy[indexOfPuppy]);
+  }
+    return res.status(400)
+    .json({
+      error: 400,
+      msg: 'Bad request, Please provide the right data'
+    });
 });
 
 app.delete('/api/puppies/:id', (req: Request, res: Response) => {
-  const id :number = Number(req.params.id)
+  const id = Number(req.params.id)
   const puppyIndex = data.findIndex(puppy => puppy.id === id);
-  data.splice(puppyIndex, 1);
-  return res.status(204).json('Puppy Succesfully deleted');
+
+  if (puppyIndex !== -1) {
+    data.splice(puppyIndex, 1);
+    return res.status(204).json('Puppy Succesfully deleted');
+  }
+  return res.status(404)
+  .json({
+    error: 404,
+    msg: 'Puppy not found'
+  });
+  
 });
 
 export default app;
